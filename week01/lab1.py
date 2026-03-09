fe7f7fa436417da0c837e02fd632daf831a6fd60
VeraBrazao <vera.afonsobrazao@georgebrown.ca>
2026-03-09 01:44:52 -0400

    Starting week 02 lab

--- week01/lab1.py
+++ week01/lab1.py
@@ -4,3 +4,26 @@
 
 # Question 2: Defining an array (list)
 my_array = [1,4,7,9]
+
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a - B ** c // d + a % c
+e = (a - ((b** C) // d)) + (a % c)
+print("Question 3 result:", e)
+
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is : {:.3f} degrees Celsius".format(temperature))
+
+
+# Question 5:
+userAge = int (input("Enter your age: "))
+
+userAge = userAge + 22
+print( "Now showhing the shop items filtered by age:", userAge)
