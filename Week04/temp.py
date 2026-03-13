a2f2bd402b80405d0b36250c1ccfbef9fbace1a9
VeraBrazao <vera.afonsobrazao@georgebrown.ca>
2026-03-13 04:49:57 -0400

    add week 04 lab04

---
+++ Week04/temp.py
@@ -0,0 +1,16 @@
+#def myFunction(): # to create a function using a define or def keyworkd, them I need my function name
+   #pass #this is the minimal fucntion definition
+#myFunction() # Call this funtion
+
+#def sum_a_b(a, b):
+    #print(a + b)
+   # return (a + b)
+
+#result = sum_a_b(1, 3) # the result is 4
+#print(result) 
+
+# - For Loops: how it works
+myList = [0, 1, 2, 3, 4] # create a list
+
+for n in myList: # show the list
+    print(n) # it will print all these numbers one each a time 
